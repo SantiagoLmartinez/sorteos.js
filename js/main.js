29db0941886f3cc5d26557ba8f974c586d0a2183
Santiago L Martinez <slm17slm17@gmail.com>
2022-02-18 03:28:17 -0300

    se mejoro la validacion y estilo

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const _formSorteo = document.getElementById('formSorteo')
 const _numGanador = document.getElementById('numGanador')
 const _numeroGanadorContainer = document.getElementById('numeroGanadorContainer')
+const _alertas = document.querySelector('.alertas')
 
 // Eventslistener
 _formSorteo.addEventListener('submit', (e)=>{
@@ -12,23 +13,27 @@ _formSorteo.addEventListener('submit', (e)=>{
 
     // Validacion de inputs
     if( min === "" ){
-        alert('No ingresaste valor minimo')
+        // alert('No ingresaste valor minimo')
+        _alertas.innerHTML = `<p class="alert alert-danger w-75">No ingresaste valor minimo</p>`
         _formSorteo.reset()
         return
     }
     if(max === ""){
-        alert('Ingresar valor maximo por favor')
+        // alert('Ingresar valor maximo por favor')
+        _alertas.innerHTML = `<p class="alert alert-danger w-75">No ingresaste valor maximo</p>`
         _formSorteo.reset()
         return
     }
     if(max < min){
-        alert('El numero minimo debe ser menor al maximo')
+        // alert('El numero minimo debe ser menor al maximo')
+        _alertas.innerHTML = `<p class="alert alert-danger w-75">El numero minimo debe ser menor al maximo</p>`
         _formSorteo.reset()
         return
     }
     
     // console.log( sortearNumeroAleatoreo(min,max))
     mostrarNumeroGanadorDom()
+    _alertas.innerHTML = ''
     _numGanador.innerHTML = `${sortearNumeroAleatoreo(min,max)}`
 
 })
@@ -39,4 +44,8 @@ const sortearNumeroAleatoreo = (min, max) => {
 // funcion para mostrar numero ganador en el DOM
 const mostrarNumeroGanadorDom = () =>{
     _numeroGanadorContainer.classList.remove('d-none')
+}
+// funcion msj alertas dom
+const alertasDom = () => {
+    // PENDIENTE
 }
