2fe97eb1889ca6df12002d3afe046be3f654c6e9
Santiago L Martinez <slm17slm17@gmail.com>
2022-02-18 01:07:52 -0300

    logaritmo para obtener numero aleatoreo terminado

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 // console.log('Conectado con html')
 const _formSorteo = document.getElementById('formSorteo')
+const _numGanador = document.getElementById('numGanador')
 
 // Eventslistener
 _formSorteo.addEventListener('submit', (e)=>{
     e.preventDefault()
-    console.log('diste click en el botton')
+    // console.log('diste click en el botton')
+    const data = new FormData(_formSorteo)
+    const [min, max] = [...data.values()]
+    
+    // console.log( sortearNumeroAleatoreo(min,max))
+    _numGanador.innerHTML = `${sortearNumeroAleatoreo(min,max)}`
+
 })
+// funcion para obtener numero aleatoreo
+const sortearNumeroAleatoreo = (min, max) => {
+    return parseInt(Math.random() * (max - min) + min);
+}
