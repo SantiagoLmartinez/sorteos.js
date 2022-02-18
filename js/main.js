d33e5d19cb0e4fd5bbcaa914fdff2cfee3e34756
Santiago L Martinez <slm17slm17@gmail.com>
2022-02-18 00:43:45 -0300

    se creo main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// console.log('Conectado con html')
+const _formSorteo = document.getElementById('formSorteo')
+
+// Eventslistener
+_formSorteo.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    console.log('diste click en el botton')
+})
